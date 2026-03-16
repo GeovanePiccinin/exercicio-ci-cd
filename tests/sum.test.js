333e3eb9c583fa37dcfb937799d09647c52517b3
Geovane <geovane.piccinin@gmail.com>
2026-03-16 17:22:12 -0300

    add number correctly

--- tests/sum.test.js
+++ tests/sum.test.js
@@ -3,3 +3,7 @@ import { sum } from "../src/sum.js";
 test("adds numbers correctly", () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+test("adds others numbers correctly", () => {
+  expect(sum(3, 2)).toBe(5);
+});
